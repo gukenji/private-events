799e262081e6331048af04568d0e1e8f873b26bb
Gustavo Takahashi <gustavokenjitakahashi@gmail.com>
2022-07-18 20:46:05 -0300

    devise gem installed and user creation configured

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,2 +1,21 @@
 class EventsController < ApplicationController
+  before_action :authenticate_user!, except: [:index, :show]
+
+  def index
+    @events = Event.all
+    @event = Event.new
+  end
+
+
+  def show
+  end
+
+  def create
+    @event = current_user.events.build(event_params)
+  end
+
+  def event_params
+    params.require(:event).permit(:date, :local, :name)
+  end
+
 end
