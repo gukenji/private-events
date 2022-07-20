d6a6d1fa5e36caeeb54fc61f8d25b7029e7b9fcd
Gustavo Takahashi <gustavokenjitakahashi@gmail.com>
2022-07-19 23:02:41 -0300

    finished index, login and signup layouts

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,21 +1,31 @@
 class EventsController < ApplicationController
-  before_action :authenticate_user!, except: [:index, :show]
+  before_action :authenticate_user!, except: %i[index show]
 
   def index
     @events = Event.all
     @event = Event.new
   end
 
-
-  def show
+  def new
+    @event = current_user.created_events.build
   end
 
+  def show; end
+
   def create
-    @event = current_user.events.build(event_params)
+    @event = current_user.created_events.build(event_params)
+    respond_to do |format|
+      if @event.save
+        format.html { redirect_to root_path, notice: 'Evento criado com sucesso!' }
+        format.json { render :show, status: :created, location: @event }
+      else
+        format.html { render :new, status: :unprocessable_entity }
+        format.json { render json: @event.errors, status: :unprocessable_entity }
+      end
+    end
   end
 
   def event_params
     params.require(:event).permit(:date, :local, :name)
   end
-
 end
